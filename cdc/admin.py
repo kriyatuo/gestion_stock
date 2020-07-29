77b5846b146d2034e70565d599f30452fff446ea
abraham <kriyatuo@gmail.com>
2020-07-29 20:00:32 +0000

    nouvelle version du fichier

--- cdc/admin.py
+++ cdc/admin.py
@@ -1,2 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import *
+
+admin.site.site_header = 'Gestionnaire de Stock'
+
+
+class LivraisonAdmin(admin.ModelAdmin):
+    list_display = ('fournisseur', 'date')
+    list_filter = ('date',)
+
+
+admin.site.register(Livraison, LivraisonAdmin)
+admin.site.register(Fournisseur)
+admin.site.register(Type_personne)
+admin.site.register(Personnes)
+admin.site.register(Medicaments)
+admin.site.register(Recuperation)
+admin.site.register(Batch)
+admin.site.unregister(Group)
--- cdc/models.py
+++ cdc/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Fournisseur(models.Model):
     code = models.CharField(max_length=25)
-    nom = models.TextField()
+    nom_fournisseur = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nom
+        return self.nom_fournisseur
 
 
 class Type_personne(models.Model):
@@ -31,8 +31,47 @@ class Personnes(models.Model):
         on_delete=models.CASCADE,
     )
     code = models.CharField(max_length=25)
-    nom = models.TextField()
-    prenom = models.TextField()
+    nom = models.CharField(max_length=40)
+    prenom = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'Nom : {self.nom}; Prenoms: {self.prenom}'
+        return '{} {}'.format({self.nom}, {self.prenom})
+
+
+class Medicaments(models.Model):
+    nom_medicament = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nom_medicament
+
+
+class Recuperation(models.Model):
+    Personnes = models.ForeignKey(
+        'Personnes',
+        on_delete=models.CASCADE,
+    )
+    Medicaments = models.ForeignKey(
+        'Medicaments',
+        on_delete=models.CASCADE,
+    )
+    date = models.DateTimeField()
+    quantite = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return '{} {} {}'.format({self.Personnes}, {self.Medicaments}, {self.quantite})
+
+
+class Batch(models.Model):
+    Livraison = models.ForeignKey(
+        'Livraison',
+        on_delete=models.CASCADE,
+    )
+    Medicaments = models.ForeignKey(
+        'Medicaments',
+        on_delete=models.CASCADE,
+    )
+    bacth_id = models.FloatField()
+    quantite = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return '{} {}'.format({self.bacth_id}, {self.Medicaments})
